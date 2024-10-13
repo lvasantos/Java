cc0bb47146827b32ca7f1b08daf767f158c9ea4d
Luciana Adriao <lucianaasantos@hotmail.com>
2024-10-13 17:42:26 +0200

    Trabalho de C++

--- TP3-cpp/include/matrix.h
+++ TP3-cpp/include/matrix.h
@@ -1,7 +1,6 @@
 
 #ifndef MATRIX_H
 #define MATRIX_H
-
 #include <iostream>
 
 class Matrix
@@ -14,55 +13,43 @@ private:
 public:
     // Constructors
     // Default constructors for 1x1 Matrix
-    Matrix() : _rows(1), _cols(1)
-    {
-        data = new float[1];
-        data[0] = 0.0f;
-    }
-
+    Matrix();
+    // Destructors
+    ~Matrix();
     // 2. Construtor com número de linhas e colunas, inicializando com 0
-    Matrix(int r, int c) : _rows(r), _cols(c)
-    {
-        data = new float[_rows * _cols];
-        std::memset(data, 0, _rows * _cols * sizeof(float)); // Inicializa tudo com 0
-    }
-
+    Matrix(int r, int c);
     // 3. Construtor com tamanho e array 1D de valores
-    Matrix(int r, int c, const float *values) : _rows(r), _cols(c)
-    {
-        data = new float[_rows * _cols];
-        std::memcpy(data, values, _rows * _cols * sizeof(float)); // Copia os valores fornecidos
-    }
+    Matrix(int r, int c, const float *values);
 
     // 4. Construtor de cópia
-    Matrix(const Matrix &other) : _rows(other._rows), _cols(other._cols)
-    {
-        data = new float[_rows * _cols];
-        std::memcpy(data, other.data, _rows * _cols * sizeof(float)); // Copia os dados da outra matriz
-    }
-
+    Matrix(const Matrix &other);
     // 5. Operador de atribuição por cópia
-    Matrix &operator=(const Matrix &other)
-    {
-        if (this != &other)
-        {                  // Verifica auto-atribuição
-            delete[] data; // Libera a memória atual
-
-            // Aloca nova memória e copia os dados
-            _rows = other._rows;
-            _cols = other._cols;
-            data = new float[_rows * _cols];
-            std::memcpy(data, other.data, _rows * _cols * sizeof(float));
-        }
-        return *this;
-    }
-    // Destructors
+    Matrix &operator=(const Matrix &other);
+    // 6. Construtor de movimentação
+    Matrix(Matrix &&otherMatrix);
+    // Operador de atribuição por movimentação
+    Matrix &operator=(Matrix &&otherMatrix);
 
     // arithmetics
+    Matrix operator+(const Matrix &m2) const;
+    Matrix operator*(const Matrix &m2) const;
+    Matrix operator-(const Matrix &m2) const;
+    Matrix operator-() const;
+
+    // TODO: operadores de >> e <<
 
     // Utils
-
-    // overload for arithmetics
+    int getRows() const;
+    int getCols() const;
+    void displayMatrix() const; // Show Matrix as Matrix
+    void display() const;       // Show Matrix as [ [0, 0], [0, 0] ]
+    const float *getData() const;
+    float *getData();
+    float *operator[](int row);             // You can write at the position
+    const float *operator[](int row) const; // Read-only
+    bool isSquare() const;
+    Matrix transpose() const;
+    Matrix identity(int size);
 };
 
 #endif // COMPLEX_H
